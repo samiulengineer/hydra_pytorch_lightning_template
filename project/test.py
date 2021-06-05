d1b779b79e45b69f42195ac182900a3c150d1fac
MD Samiul Islam <samiulengineer@gmail.com>
2021-06-05 13:51:30 -0600

    .

--- project/test.py
+++ project/test.py
@@ -3,45 +3,45 @@
 # File  : test.py
 # Author: Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
 # Date  : 02.05.2021
-
+  
 import logging
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
-import pytorch_lightning as pl
-import torch
+import hydra                                                                        # hydar 
+from omegaconf import DictConfig, OmegaConf                                         # omegaconf and Dictconfig are used for the control of the yaml file 
+import pytorch_lightning as pl                                                      # pytorch lightning 
+import torch                                                                        # pytorch
 import json
 
-log = logging.getLogger(__name__)
+log = logging.getLogger(__name__)                                                   # to store the logg info 
 
-
+#hydar incitialization with with hydar decorator .. pointing to the config folder and test_config.yaml file 
 @hydra.main(config_path='config', config_name='test_config')
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
-    pl.seed_everything(cfg.seed)
+    pl.seed_everything(cfg.seed)                                                    # Function that sets seed for pseudo-random number generators in: pytorch, numpy, python.random In addition, sets the following environment variables:
 
     # ------------
     # data
     # ------------
-    data_module = hydra.utils.instantiate(cfg.data) 
-    test_dataloader = data_module.test_dataloader()
+    data_module = hydra.utils.instantiate(cfg.data)                                  # create an instance of class 
+    test_dataloader = data_module.test_dataloader()                                  # calling the test_dataloader function  from class to lead data 
 
     # ------------
     # model
     # ------------
-    model = hydra.utils.instantiate(cfg.model) 
+    model = hydra.utils.instantiate(cfg.model)                                       # create an instance of class  
     # model.load_from_checkpoint(cfg.checkpoint)
-    model.load_state_dict(torch.load(cfg.checkpoint_path)['state_dict'])
+    model.load_state_dict(torch.load(cfg.checkpoint_path)['state_dict'])             # cfg.chackpoint_path = placeholder for pretrained checkpoint
 
-    trainer = pl.Trainer(**(cfg.pl_trainer))
+    trainer = pl.Trainer(**(cfg.pl_trainer))                                         # start tanning  with Trainer function 
 
     # ------------
     # testing
     # ------------
-    result = trainer.test(model, test_dataloaders=test_dataloader)
-    log.info(result)
-    with open(f'{trainer.log_dir}/out', 'w') as f:
-        f.write(json.dumps(str(result),indent=4))
+    result = trainer.test(model, test_dataloaders=test_dataloader)                   # run test on trained model and massure the performence on test_dataloader 
+    log.info(result)                                                                 # it store all the data of true positive ,  
+    with open(f'{trainer.log_dir}/out', 'w') as f:                                   # open log file in log/run/out as write mode 
+        f.write(json.dumps(str(result),indent=4))                                    # dumps log info 
 
 
 if __name__ == '__main__':
--- project/train.py
+++ project/train.py
@@ -5,14 +5,14 @@
 # Date  : 02.05.2021
 import logging
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
-import pytorch_lightning as pl
+import hydra                                                                        # hydra 
+from omegaconf import DictConfig, OmegaConf                                         # dirctconfig and omegaconfig 
+import pytorch_lightning as pl                                                      # pytorch lighinging 
 
-log = logging.getLogger(__name__)
+log = logging.getLogger(__name__)                                                   # logger
 
 
-@hydra.main(config_path='config', config_name='train_config')
+@hydra.main(config_path='config', config_name='train_config')                       # hydra decoretor pointing to folder config and traing_config.yaml file 
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
     pl.seed_everything(cfg.seed)
@@ -20,22 +20,22 @@ def main(cfg: DictConfig):
     # ------------
     # data
     # ------------
-    data_module = hydra.utils.instantiate(cfg.data)
+    data_module = hydra.utils.instantiate(cfg.data)                                 # creating a instance of mnist_data_module.py 
 
 
     # ------------
     # model
     # ------------
-    model = hydra.utils.instantiate(cfg.model)
+    model = hydra.utils.instantiate(cfg.model)                                       # creating instance of dncnn.py 
 
     # ------------
     # training
     # ------------
-    trainer = pl.Trainer(**(cfg.pl_trainer), checkpoint_callback=True)
-    log.info('run training...')
-    train_dataloader = data_module.train_dataloader()
+    trainer = pl.Trainer(**(cfg.pl_trainer), checkpoint_callback=True)               # creating a instance of Trainer class with gpu = 1
+    log.info('run training...')                                                      # store the logging info fo Trainer class
+    train_dataloader = data_module.train_dataloader()                                # calling the train dataloader from 
     val_dataloader = data_module.val_dataloader()
-    trainer.fit(model, train_dataloader=train_dataloader, val_dataloaders=[val_dataloader])
+    trainer.fit(model, train_dataloader=train_dataloader, val_dataloaders=[val_dataloader])  # start the training 
 
 
 if __name__ == '__main__':
